f90c2f5f7c47a0539bb93ce7ee9804a4b921836f
Mike Seese <seesemichaelj@gmail.com>
2020-11-04 17:31:50 -0800

    simplify contract

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -3,22 +3,22 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TruffleBadge is ERC721 {
+contract TruffleConBadge is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _badgeIds;
 
-    constructor() ERC721("TruffleBadge", "TRFL") public {
+    constructor() ERC721("TruffleCon", "TRFL") public {
     }
-    
-    function awardBadge(address attendee, string memory attendenceBadgeURI)
+
+    function claimBadge(string memory attendenceBadgeURI)
         public
         returns (uint256)
     {
         _badgeIds.increment();
 
         uint256 newBadgeId = _badgeIds.current();
-        _mint(attendee, newBadgeId);
-        _setTokenURI(newBadgeId, attendenceBadgeURI);
+        _mint(msg.sender, newBadgeId);
+        _setTokenURI(newBadgeId, "https://ipfs.infura-ipfs.io/ipfs/QmQcqMmMx1vn8FoN2RtPubsSpKWTFAWsE1p6hdvwv7ofo8/badge1.png");
 
         return newBadgeId;
     }
