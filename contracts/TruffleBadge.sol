43324d49616a9794f07c86fd2f661dfb5233d317
Mike Seese <seesemichaelj@gmail.com>
2020-11-04 18:05:19 -0800

    use a uint256 instead of a counter

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -1,23 +1,22 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TruffleBadge is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _badgeIds;
+    uint256 public nextBadgeId;
 
     constructor() ERC721("TruffleBadge", "TRFL") public {
+        nextBadgeId = 0;
     }
 
     function claimBadge(string memory attendenceBadgeURI)
         public
         returns (uint256)
     {
-        _badgeIds.increment();
-
-        uint256 newBadgeId = _badgeIds.current();
+        uint256 newBadgeId = nextBadgeId;
         _mint(msg.sender, newBadgeId);
+        nextBadgeId = nextBadgeId + 1;
+
         _setTokenURI(newBadgeId, "https://ipfs.io/ipfs/QmP6AFJbppxmebAgsQJ6mNeBKe22VZGhMzBsSU143XfmiB/meta1.json");
 
         return newBadgeId;
